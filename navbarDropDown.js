19711a6a8167bc0076f08a94701fc4d0ad0cb309
likhon15 <likhon15-3916@diu.edu.bd>
2024-03-14 06:51:22 +0600

    added

---
+++ navbarDropDown.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Add a click event listener to the button
+    $('#dropdownHoverButton1').on('click', function() {
+      // Toggle the visibility of the dropdown menu
+      $('#dropdownHover1').toggleClass('hidden');
+    });
+  });
